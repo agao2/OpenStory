99315e25aff342662a99f74bf2890d5fa39fec99
Alex <gaoalexander73@gmail.com>
2018-05-21 21:32:27 -0700

    Add project files.

---
+++ OpenStory/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(OpenStory.Startup))]
+namespace OpenStory
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
