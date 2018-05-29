fbce283c7108d001aa05afcd1691ef60b5d5bb23
Alex <gaoalexander73@gmail.com>
2018-05-28 19:20:27 -0700

    my posts page
    interface to view all of a users post and replies

---
+++ OpenStory/Controllers/MyPostsController.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Microsoft.AspNet.Identity;
+using Microsoft.AspNet.Identity.EntityFramework;
+using OpenStory.Models;
+using System.Data.Entity;
+
+namespace OpenStory.Controllers
+{
+    public class MyPostsController : Controller
+    {
+
+        private ApplicationDbContext _context;
+
+        public MyPostsController()
+        {
+            _context = new ApplicationDbContext();
+        }
+
+        [Authorize]
+        public ActionResult Index()
+        {
+            string userId = User.Identity.GetUserId();
+
+            IEnumerable<Topic> topics = from topic in _context.Topics.Include(t => t.ApplicationUser)
+                         where topic.ApplicationUser.Id == userId
+                         select topic;
+            IEnumerable<Reply> replies = from reply in _context.Replies.Include(r => r.ApplicationUser).Include(r => r.Topic)
+                                         where reply.ApplicationUser.Id == userId
+                                         select reply;
+
+
+            MyPostsViewModel viewModel = new MyPostsViewModel()
+            {
+                Topics = topics,
+                Replies = replies
+            };
+
+            return View(viewModel);
+        }
+    }
+}
---
+++ OpenStory/Models/MyPostsViewModel.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace OpenStory.Models
+{
+    public class MyPostsViewModel
+    {
+        public IEnumerable<Topic> Topics { get; set; }
+        public IEnumerable<Reply> Replies { get; set; }
+    }
+
+    public class PostPartialViewModel
+    {
+        public string Title { get; set; }
+        public string Content { get; set; }
+        public int Id { get; set; }
+        public string Username { get; set; }
+        public DateTime PostDate { get; set; }
+    }
+}
